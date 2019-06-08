51c9a04b2fb9010c2299054d19927946476f66ba
Zachary Burke <zachb89@gmail.com>
2019-06-07 23:16:58 -0500

    3 - Functional Components

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -7,7 +8,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header />
         <Contact />
       </div>
     );
