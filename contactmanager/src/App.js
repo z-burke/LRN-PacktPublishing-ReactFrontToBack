51431d1845e8ce70497566ed00a94ca1534027fa
Zachary Burke <zachb89@gmail.com>
2019-06-07 22:34:04 -0500

    What Is JSX?

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>The App Component</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
