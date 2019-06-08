50be1b9dd845297e39fb7b67ca0e60914198958e
Zachary Burke <zachb89@gmail.com>
2019-06-08 16:17:54 -0500

    4 - Creating State

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
     render() {
-        const { name, email, phone } = this.props;
+        const { name, email, phone } = this.props.contact;
         return (
             <div className="card card-body mb-3">
                 <h4>{name}</h4>
@@ -12,14 +12,12 @@ class Contact extends Component {
                     <li className="list-group-item">Phone: {phone}</li>
                 </ul>
             </div>
-        )
+        );
     }
 }
 
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
+    contact: PropTypes.object.isRequired,
 }
 
 export default Contact;
