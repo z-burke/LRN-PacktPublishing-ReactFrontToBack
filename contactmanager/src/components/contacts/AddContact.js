00e70992f5ccf2d0db4c9ece1922eb00b2a61bcd
Zachary Burke <zachb89@gmail.com>
2019-06-08 21:51:44 -0500

    5 - Controlled Components & onChange

---
+++ contactmanager/src/components/contacts/AddContact.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+class AddContact extends Component {
+    state = {
+        name: '',
+        email: '',
+        phone: ''
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+    
+    render() {
+        const { name, email, phone } = this.state;
+        return (
+            <div className="card mb-3">
+                <div className="card-header">Add Contact</div>
+                <div className="card-body">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Name..."
+                                value={name}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Email..."
+                                value={email}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone</label>
+                            <input
+                                type="text"
+                                name="phone"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Phone..."
+                                value={phone}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AddContact;
