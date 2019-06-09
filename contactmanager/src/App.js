640c65a383620ade188a13852144c840a00fd7a0
Zachary Burke <zachb89@gmail.com>
2019-06-09 09:46:45 -0500

    6 - 404 NotFound Page

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -4,6 +4,7 @@ import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import { Provider } from './context';
 
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exacat path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>              
             </div>
           </div>
